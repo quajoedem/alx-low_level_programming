5f605700be8c98cba21cf0b06cd7f94ae09cafd5
Ernest Avorwlanu <quajoedem@gmail.com>
2023-06-15 19:14:56 -0700

    C hello world

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,7 +4,7 @@
  * Return: Always 0
  */
 int main(void)
-[
+{
 	printf("Size of a char: %i byte(s)\n", sizeof(char));
 	printf("Size of an int: %i byte(s)\n", sizeof(int));
 	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
